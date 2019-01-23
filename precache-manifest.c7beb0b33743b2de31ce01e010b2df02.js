2fd90441013a3f9376fcb352a817af298990b688
Donata Mielaikaite <d.mielaikaite@gmail.com>
2019-01-23 20:53:59 +0100

    Updates

---
+++ precache-manifest.c7beb0b33743b2de31ce01e010b2df02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5df9bc9b1a73480a796d",
+    "url": "/static/css/main.bf9d51c7.chunk.css"
+  },
+  {
+    "revision": "5df9bc9b1a73480a796d",
+    "url": "/static/js/main.5df9bc9b.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "694fd56fcc615f51c4573c083e75e9e7",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/resume/precache-manifest.4348b2290cbc30304a306c1464510ea3.js"
+  "/precache-manifest.c7beb0b33743b2de31ce01e010b2df02.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/resume/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
